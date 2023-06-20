b8c9b794f07251d652b2987ff30f7f6218d26703
nubsaybot227 <sinkevicmihail09@gmail.com>
2023-06-20 15:39:31 +0300

    version2

---
+++ js.js
@@ -0,0 +1,11 @@
+document.getElementById('stripes').onclick = fun;
+document.getElementById('cross').onclick = close;
+
+function fun(){
+    document.getElementById('menu').style.left = 0;
+}
+
+function close(){
+    document.getElementById('menu').style.left = -300 + 'px';
+}
+
