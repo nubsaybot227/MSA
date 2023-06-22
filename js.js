16ea069d91909f7f516e47d3bebac8da5e72d175
nubsaybot227 <sinkevicmihail09@gmail.com>
2023-06-22 16:11:16 +0300

    version3

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
-document.getElementById('stripes').onclick = fun;
-document.getElementById('cross').onclick = close;
+// document.getElementById('stripes').onclick = fun;
+// document.getElementById('cross').onclick = close;
 
-function fun(){
-    document.getElementById('menu').style.left = 0;
-}
+// function fun(){
+//     document.getElementById('menu').style.left = 0;
+// }
 
-function close(){
-    document.getElementById('menu').style.left = -300 + 'px';
-}
+// function close(){
+//     document.getElementById('menu').style.left = -300 + 'px';
+// }
 
